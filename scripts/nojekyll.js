ea782a6428269e9317739e122a0442d905771f70
Kevin To <qkevinto@gmail.com>
2016-04-28 23:30:42 +1000

    Remove .nojekyll from source as it does not get copied over to .deploy_git during deployment as it is a hidden file, instead write a short script to create .nojekyll in the .deploy_git folder before deployment.

---
+++ scripts/nojekyll.js
@@ -0,0 +1,16 @@
+/**
+ * hexo-deployer-git uses hexo-fs copyDir() method which has ignoreHidden set
+ * to true by default, hence .nojekyll file doesn't get copied from public/ even
+ * if it is created.
+ *
+ * This script simply creates a .nojekyll in .deploy_git if the folder exists
+ */
+
+const fs = require('hexo-fs');
+
+hexo.on('deployBefore', () => {
+  fs.writeFile('.deploy_git/.nojekyll', '')
+    .then(() => {
+      console.log('nojekyll.js: .nojekyll file created in .deploy_git');
+    });
+});
