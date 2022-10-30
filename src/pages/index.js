2392a35280dd797b6ab17e3ce9eec56ee88b2629
Kevin To <qkevinto@gmail.com>
2022-10-30 21:22:36 +1100

    Move to GraphQl and Gatsby Head for site meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,62 @@
 import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby'
 
 import Layout from '../components/Layout';
 import Root from '../components/Root';
+
+import '../css/main.scss';
+
+export const Head = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          shortName
+          url
+          author
+          keyword
+          image
+          themeColor
+          twitterHandle
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <title>{data.site.siteMetadata.title}</title>
+
+      <meta name="description" content={data.site.siteMetadata.description} />
+      <meta name="keywords" content={data.site.siteMetadata.keywords} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="author" content={data.site.siteMetadata.author} />
+      <meta name="apple-mobile-web-app-title" content={data.site.siteMetadata.title} />
+      <meta name="application-name" content={data.site.siteMetadata.title} />
+      <meta name="msapplication-TileColor" content={data.site.siteMetadata.themeColor} />
+      <meta name="msapplication-TileImage" content={'/mstile-144x144.png'} />
+      <meta name="theme-color" content={data.site.siteMetadata.themeColor} />
+      <meta name="twitter:card" content={'summary'} />
+      <meta name="twitter:site" content={`@${data.site.siteMetadata.twitterHandle}`} />
+      <meta name="twitter:title" content={data.site.siteMetadata.title} />
+      <meta name="twitter:description" content={data.site.siteMetadata.description} />
+      <meta name="twitter:image" content={`${data.site.siteMetadata.url}${data.site.siteMetadata.image}`} />
+      <meta property="og:title" content={data.site.siteMetadata.title} />
+      <meta property="og:type" content='website' />
+      <meta property="og:url" content={data.site.siteMetadata.url} />
+      <meta property="og:image" content={`${data.site.siteMetadata.url}${data.site.siteMetadata.image}`} />
+
+      <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192" />
+      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+      <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" />
+      <link rel="canonical" href={data.site.siteMetadata.url} />
+    </>
+  )
+}
 
 const Indexpage = () => {
   return (
